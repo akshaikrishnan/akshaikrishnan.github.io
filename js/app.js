212370c6e8cbe1194f9ce5f05bcd07a75fb97856
scribblepanda <akshaik143@gmail.com>
2022-02-11 12:36:37 +0530

    changes

--- js/app.js
+++ js/app.js
@@ -104,7 +104,8 @@ $(".mfp-image").magnificPopup({
 $(function () {
   $("#cv").on("click", function (e) {
     e.preventDefault();
-    var url = "https://bit.ly/3hCeh5y";
+    var url =
+      "https://www.dropbox.com/s/qpp9d6z29p8qqu7/Akshai%20Krishnan%20UI%20Resume.pdf?dl=1";
     window.open(url, "_self");
   });
 });
@@ -116,4 +117,92 @@ $(function () {
     console.log(url);
     window.open(url, "_blank");
   });
+});
+
+function getAge(dateString) {
+  var now = new Date();
+  var today = new Date(now.getYear(), now.getMonth(), now.getDate());
+
+  var yearNow = now.getYear();
+  var monthNow = now.getMonth();
+  var dateNow = now.getDate();
+
+  var dob = new Date(
+    dateString.substring(6, 10),
+    dateString.substring(0, 2) - 1,
+    dateString.substring(3, 5)
+  );
+
+  var yearDob = dob.getYear();
+  var monthDob = dob.getMonth();
+  var dateDob = dob.getDate();
+  var age = {};
+  var ageString = "";
+  var yearString = "";
+  var monthString = "";
+  var dayString = "";
+
+  var yearAge = yearNow - yearDob;
+
+  if (monthNow >= monthDob) var monthAge = monthNow - monthDob;
+  else {
+    yearAge--;
+    var monthAge = 12 + monthNow - monthDob;
+  }
+
+  if (dateNow >= dateDob) var dateAge = dateNow - dateDob;
+  else {
+    monthAge--;
+    var dateAge = 31 + dateNow - dateDob;
+
+    if (monthAge < 0) {
+      monthAge = 11;
+      yearAge--;
+    }
+  }
+
+  age = {
+    years: yearAge,
+    months: monthAge,
+    days: dateAge,
+  };
+
+  if (age.years > 1) yearString = " years";
+  else yearString = " year";
+  if (age.months > 1) monthString = " months";
+  else monthString = " month";
+  if (age.days > 1) dayString = " days";
+  else dayString = " day";
+
+  if (age.years > 0 && age.months > 0 && age.days > 0)
+    ageString =
+      age.years +
+      yearString +
+      ", " +
+      age.months +
+      monthString +
+      ", and " +
+      age.days +
+      dayString;
+  else if (age.years == 0 && age.months == 0 && age.days > 0)
+    ageString = "Only " + age.days + dayString + " old!";
+  else if (age.years > 0 && age.months == 0 && age.days == 0)
+    ageString = age.years + yearString + " old. Happy Birthday!!";
+  else if (age.years > 0 && age.months > 0 && age.days == 0)
+    ageString =
+      age.years + yearString + " and " + age.months + monthString + " old.";
+  else if (age.years == 0 && age.months > 0 && age.days > 0)
+    ageString =
+      age.months + monthString + " and " + age.days + dayString + " old.";
+  else if (age.years > 0 && age.months == 0 && age.days > 0)
+    ageString =
+      age.years + yearString + " and " + age.days + dayString + " old.";
+  else if (age.years == 0 && age.months > 0 && age.days == 0)
+    ageString = age.months + monthString + " old.";
+  else ageString = "Oops! Could not calculate age!";
+
+  return ageString;
+}
+$(window).on("load", function () {
+  $("#years").html(getAge("07/06/2017"));
 });
